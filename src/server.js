0f2e5e9e767843372a766aab00d21d43999912cb
dawwson <shines1427@gmail.com>
2024-01-25 00:49:22 +0900

    Feat: express app에 pug 템플릿 엔진 설정

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen("3000");
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // 다른 url로 접근해도 "/"로 리다이렉트 해줌
+
+app.listen("3000", () => {
+  console.log("Listening on 3000");
+});
